4b00d01d4df72e9eadc010777433e1505e3c8ed4
nanaelie <elienana92@gmail.com>
2025-06-08 18:16:06 +0000

    chore: ajout de app/(tabs)/panier.tsx

---
+++ app/(tabs)/panier.tsx
@@ -0,0 +1,34 @@
+import { Image } from 'expo-image';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+
+export default function Panier() {
+	return (
+		<ScrollView
+			contentContainerStyle={styles.content}
+			style={styles.panier}
+		>
+		<Text style={styles.text}>Panier</Text>
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	content: {
+		alignItems: 'center',
+	},
+	panier: {
+		width: '100%',
+		minHeight: '100vh',
+		paddingTop: 70,
+		paddingBottom: 15,
+		display: 'flex',
+		flexDirection: 'column',
+		backgroundColor: '#fff',
+	},
+	
+	text: {
+		fontSize: 28,
+		fontWeight: 900,
+		color: '#5c4430',
+	}
+});
